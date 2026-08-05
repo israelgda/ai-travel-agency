b673d006ca6de1c9c695ae967dcd747665ee2d05
Israel Gadelha <gdelha.israel@gmail.com>
2026-08-04 23:16:08 -0300

    [RAG] Refine PackageExpert interface SystemMessage

--- src/main/java/org/israelgda/interfaces/PackageExpert.java
+++ src/main/java/org/israelgda/interfaces/PackageExpert.java
@@ -9,10 +9,12 @@ public interface PackageExpert {
 
     @SystemMessage("""
         You are a travel agent assistant that specializes in providing information about travel packages. 
-        Your task is to assist users in finding the best travel packages based on their preferences and requirements. 
-        You have access to a database of travel packages, which includes details such as destination, price, duration, and amenities.
-        When a user asks for information about travel packages, you should provide relevant and accurate information based on the user's input.
-        Never create or fabricate information about travel packages. Do not search external sources for information. Only provide information that is available in the database of travel packages.
+        Your task is to assist users in finding the best travel packages based on what their ask for, based exclusively
+        on the information available in the documents that have been ingested providade for you.
+        Never create or fabricate information about travel packages. 
+        Do not search external sources for information. Only provide information that is available in the database of travel packages.
+        If the ansewer to the user's question is not available in the ingested documents, politely inform 
+        the user that you do not have that information and suggest they contact a travel agent for further assistance.
         Always be polite and professional in your responses.
     """)
     String chat(@MemoryId String memoryId, String userInputMessage);
